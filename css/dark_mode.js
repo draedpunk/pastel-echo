d10fc84437a58d18493cddf4fb9e55e41cbd9c7a
Pamela Rodrigues <junmyeonradpet@gmail.com>
2025-03-07 01:54:56 -0300

    c5

--- css/dark_mode.js
+++ css/dark_mode.js
@@ -1,12 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
     const socialsDiv = document.getElementById("socials");
-    const themeIcon = socialsDiv.querySelector("i.fa-solid"); // Pega o ícone dentro da div #socials
+    const themeIcon = socialsDiv.querySelector("i.fa-solid");
     const body = document.body;
 
     themeIcon.addEventListener("click", function () {
-        body.classList.toggle("dark-mode"); // Alterna o tema escuro
+        body.classList.toggle("dark-mode"); 
 
-        // Alterna os ícones entre lua (noturno) e sol (claro)
         if (body.classList.contains("dark-mode")) {
             themeIcon.classList.replace("fa-moon", "fa-sun"); 
         } else {
